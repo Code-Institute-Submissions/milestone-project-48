a7362cb702e7e3cb149ad11b9d64fec3518f0ec8
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-04-28 19:47:17 +0200

    added background image to platformStartScene

--- assets/js/platform/StartScenePlatform.js
+++ assets/js/platform/StartScenePlatform.js
@@ -3,8 +3,14 @@ class StartScenePlatform extends Phaser.Scene {
         super({ key: 'StartScenePlatform' })
     }
 
+    preload() {
+        this.load.image('background', '/milestone-project-2/assets/pics/skyBackground.png');
+    }
+
     create() {
-        this.add.text(100, 250, 'Click to start!', { fill: '#fff', fontSize: '35px' });
+        const bg = this.add.image(250, 250, 'background').setScale(.3).setDepth(-1);
+
+        this.add.text(100, 250, 'Click to start!', { fill: '#000000', fontSize: '35px' });
         this.input.on('pointerdown', () => {
             this.scene.stop('StarScenePlatform')
             this.scene.start('GameScenePlatform')
